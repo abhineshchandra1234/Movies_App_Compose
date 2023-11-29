7b90b0120d9feb68e3209ac9d33a4c3b900c4c81
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-11-29 11:22:40 +0530

    Initial project setup done

---
+++ app/build.gradle.kts
@@ -0,0 +1,113 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    //id ("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.example.moviesappcompose"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.moviesappcompose"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        val compose_version = "1.4.3"
+        kotlinCompilerExtensionVersion = compose_version
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+    val coroutine_android = "1.6.1"
+    val coroutine_core = "1.6.4"
+    val moshi = "1.13.0"
+    val hilt_version = "2.39.1"
+    val retrofit = "2.9.0"
+    val lifecycle_version = "2.6.0-alpha01"
+
+    //kotlin Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutine_android")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutine_core")
+
+    // ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofit")
+    //moshi
+    implementation ("com.squareup.moshi:moshi-kotlin:$moshi")
+    implementation ("com.squareup.retrofit2:converter-moshi:$retrofit")
+
+    //dagger hilt
+    implementation ("com.google.dagger:hilt-android:$hilt_version")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+    kapt ("com.google.dagger:hilt-compiler:$hilt_version")
+
+    //hilt viewmodel
+    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    //navigation
+    implementation("androidx.navigation:navigation-compose:2.5.1")
+
+    //ViewModels
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.5.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+
+    implementation("io.coil-kt:coil-compose:2.1.0")
+}
