d8a62b41117a67423e8ee74be49931c2b2820730
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-12-07 21:05:16 +0530

    updated coil lib version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,5 +107,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
     //coil
-    implementation("io.coil-kt:coil-compose:2.1.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
